315860b3d2dacbf4b5d171d644ecaac0a533bcc6
Akihiro Komori <akihiro.komori@unity3d.com>
2021-10-06 21:18:01 +0900

    clean up

--- ROS/src/raspimouse_sim/raspimouse_gazebo/materials/gen_maze.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -eu
-
-WORKINGDIR="$(rospack find raspimouse_gazebo)"
-
-rosrun xacro xacro --inorder $WORKINGDIR/materials/sample_maze.world.xacro > $WORKINGDIR/worlds/sample_maze.world
