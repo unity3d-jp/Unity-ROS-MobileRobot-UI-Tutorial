d22e637fe1eea3ecc3f9fe6b56a79fed5c07f112
Daisuke Sato <daisuke.sato@rt-net.jp>
2022-06-21 19:41:26 +0900

    Upgrade packages
    * Unity 2020.3.17f1 to 2021.3.4f1
    * URDF Importer 0.5.0 to 0.5.2
    * ROS-TCP-Connector 0.5.0 to 0.7.0
      * https://github.com/Unity-Technologies/ROS-TCP-Connector/pull/191
    * ROS-TCP-Endpoint 0.5.0 to 0.7.0
      * https://github.com/Unity-Technologies/ROS-TCP-Endpoint/pull/127

--- ROS/configure.sh
+++ ROS/configure.sh
@@ -2,7 +2,6 @@
 
 ROS_IP=$(hostname -i)
 
-echo "ROS_IP: $ROS_IP" > $(rospack find ros_tcp_endpoint)/config/params.yaml
 sed -i -e "s/127\.0\.0\.1/$ROS_IP/g" $HOME/.bashrc
 if [ -e $HOME/.ignition/fuel/config.yaml ]; then
     sed -i -e "s/ignitionfuel\.org/ignitionrobotics.org/g" $HOME/.ignition/fuel/config.yaml
